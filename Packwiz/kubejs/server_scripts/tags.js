542e6af30ddad905c8fae6dded62661a51a35df3
JitterDev <xdc20808@gmail.com>
2024-04-18 13:03:18 -0500

    tetra import + start of materials/compat

--- Packwiz/kubejs/server_scripts/tags.js
+++ Packwiz/kubejs/server_scripts/tags.js
@@ -1,8 +1,13 @@
 
-ServerEvents.tags('block', event => 
-{
+ServerEvents.tags('block', event => {
     event.add('tfc:toughness_1', '@create')
 
     event.remove('tfc:toughness_1', 'create:controller_rail')
 	
+})
+ServerEvents.tags('item', event => {
+    const tfc_knives = event.get('forge:tools/knives').getObjectIds()
+    tfc_knives.forEach(id => {
+        event.add('c:tools/knives')
+    })
 })
